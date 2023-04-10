fcd9948cc3ce8b40131e4899a55558c623c0c7ba
Alexander <Ra1jinThunderkeg@yandex.ru>
2023-04-10 14:43:11 +0300

    second step fix gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,5 +4,8 @@ import { program } from 'commander';
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
- 
+  .argument('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
+  .helpOption('-h, --help', 'output usage information');
+
 program.parse();
