efae3d7b6fd139ceab5d14dc88fbf01532aa1e0e
MorbidDesire <124774266+MorbidDesire@users.noreply.github.com>
2023-04-16 12:46:24 +0300

    Update index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,13 @@
 import { test, expect } from '@jest/globals';
+import { fileURLToPath } from 'node:url';
+import path from 'path';
 import genDiff from '../src/index.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
 test('gendiff', () => {
-  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual('{"- follow":false,"  host":"hexlet.io","- proxy":"123.234.53.22","- timeout":50,"+ timeout":20,"+ verbose":true}');
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual('{"- follow":false,"  host":"hexlet.io","- proxy":"123.234.53.22","- timeout":50,"+ timeout":20,"+ verbose":true}');
 });
